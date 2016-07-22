bbdd435c160be6d85f3ff255f41f997f5092c938
Gil Nahmias <gilna@Gils-MacBook-Pro.local>
2016-07-21 19:28:30 -0700

    refactor for readability

--- nextpoop.rb
+++ nextpoop.rb
@@ -1,19 +1,20 @@
 require 'pcaprub'
+require 'time'
 require_relative './EthernetFrame'
 require_relative './log'
-require 'time'
 
-capture = PCAPRUB::Pcap.open_live('en0', 65535, true, 0)
-capture.setfilter('arp')
-
+OUR_WIFI_INTERFACE = "en0"
 NEW_CONNECTION = "00:00:00:00:00:00"
 DASH_MILK_BABY = "f0:27:2d:41:cd:83"
 
 puts "Started at " + Time::now.to_s
 
+capture = PCAPRUB::Pcap.open_live(OUR_WIFI_INTERFACE, 65535, true, 0)
+capture.setfilter('arp')
+
 loop do
   while (pkt = capture.next())
-    packet = EthernetFrame.parse(pkt).payload.output #.inspect
+    packet = EthernetFrame.parse(pkt).payload.output
     sender = packet[:sender_hw_addr]
     target = packet[:target_hw_addr]
 
